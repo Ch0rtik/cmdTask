11651049ea1a9b3fc087a3b28155c9a37c2aea6f
Ch0rtik <10raven11@gmail.com>
2022-04-08 16:41:46 +0300

    Tests

---
+++ src/test/java/uniq/MergerTests.java
@@ -0,0 +1,78 @@
+package uniq;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MergerTests {
+    final String RESOURCE_PATH = "src/test/resources/";
+    private void assertFileOutput(String[] args, File expected) throws IOException {
+        File tempOut = new File(RESOURCE_PATH +"tempOut.txt");
+        tempOut.createNewFile();
+        MergerLauncher.main(args);
+
+        assertTrue(FileUtils.contentEquals(tempOut, expected));
+
+        tempOut.delete();
+    }
+
+    private void assertConsoleOutput(String[] args, String expected) {
+        String consoleOutput = "";
+        try {
+            consoleOutput = tapSystemOut(() -> MergerLauncher.main(args));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        assertEquals(expected, consoleOutput.trim());
+    }
+
+    @Test
+    void fileInput() throws IOException {
+        assertFileOutput(new String[]{"-o", RESOURCE_PATH + "tempOut.txt", RESOURCE_PATH + "In.txt"},
+                new File(RESOURCE_PATH +"simpleOut.txt"));
+    }
+
+    @Test
+    void withCounter() throws IOException {
+        assertFileOutput(new String[]{"-o", RESOURCE_PATH + "tempOut.txt", "-c", RESOURCE_PATH + "In.txt"},
+                new File(RESOURCE_PATH +"countOut.txt"));
+    }
+
+    @Test
+    void ignoreCase() throws IOException {
+        assertFileOutput(new String[]{"-o", RESOURCE_PATH + "tempOut.txt", "-i", RESOURCE_PATH + "In.txt"},
+                new File(RESOURCE_PATH +"ignoreOut.txt"));
+
+    }
+
+    @Test
+    void consoleInput() throws IOException {
+        assertFileOutput(new String[]{"-o", RESOURCE_PATH + "tempOut.txt", "-i",
+                "Hello, world!\n" +
+                        "Hello, world!!!\n" +
+                        "hello, world!\n" +
+                        "hello, world!\n" +
+                        "hello, World!\n" +
+                        "Hell , World!"}, new File(RESOURCE_PATH +"ignoreOut.txt"));
+    }
+
+    @Test
+    void withSkip() throws IOException{
+        assertFileOutput(new String[]{"-o", RESOURCE_PATH + "tempOut.txt", "-s", "5", RESOURCE_PATH + "In.txt"},
+                new File(RESOURCE_PATH +"skipOut.txt"));
+    }
+
+    @Test
+    void consoleOutput() throws IOException {
+        assertConsoleOutput(new String[]{"-s", "5", "-i", RESOURCE_PATH + "In.txt"},
+                "Hello, world!\r\n" +
+                        "Hello, world!!!\r\n" +
+                        "hello, world!");
+    }
+}
